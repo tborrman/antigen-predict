8ccc8bcd87d5f970a82ec110bbf594d3dc27975e
tborrman <tylerborrman@gmail.com>
2015-08-06 15:28:17 -0400

    add 3QIU and 1YMM related code

---
+++ 3QIU_process_step1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+from Bio import SeqIO
+from Bio.Alphabet import IUPAC
+import re
+
+def main():
+	parser = argparse.ArgumentParser(description= 'Deconvolute Birnbaum seq data into rounds of selection from specific library')
+	parser.add_argument('-f', help='fastq file (must be only single reads or single mates from paired end reads)', dest='fastq', type=str, required=True)
+	parser.add_argument('-b', help=' 6 nucleotide dna barcode for selection round and library', type=str, dest='barcode', required=True)
+
+	args=parser.parse_args()
+	fastq=args.fastq
+	barcode=args.barcode
+
+	sel = 0 
+	tot = 0
+
+	sel_seq = barcode + 'CTGTTATTGCTAGCGTTTTAGCA'
+	
+	OUT = open('3QIU_process_step1.fastq', 'w+')
+	for seq_record in SeqIO.parse(fastq, 'fastq', IUPAC.ambiguous_dna):
+		tot += 1
+		if tot % 100000 == 0:
+			print 'On read:' + str(tot)
+		if re.search(sel_seq, str(seq_record.seq)):
+			sel += 1
+			OUT.write(seq_record.format('fastq'))
+	print 'Total reads processed: ' + str(tot)
+	print 'Total reads selected: ' + str(sel)
+	print 'Percentage: ' + str((float(sel)/float(tot)) * 100) + '%'
+	OUT.close()
+
+
+if __name__ == '__main__':
+	main()
+	
