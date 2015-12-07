e3d43bd8aed5e10944599c2b4f7a3f51994eae3a
tborrman <tylerborrman@gmail.com>
2015-12-07 18:41:42 -0500

    figure 1&2

--- SPR_plot.R
+++ SPR_plot.R
@@ -2,23 +2,45 @@
 library(argparse)
 
 parser <- ArgumentParser(description="Plot scores vs SPR energies")
-parser$add_argument("-pdb", help= "pdb ID (3QIB)", type="character", dest="pdb", required=TRUE)
+#parser$add_argument("-pdb", help= "pdb ID (3QIB)", type="character", dest="pdb", required=TRUE)
 
 args <- parser$parse_args()
 wkdir <- getwd()
 
 # Open files
-dG_df <- read.table(paste(wkdir, "/", args$pdb, "_dGs.txt", sep=""), header=TRUE, sep="\t")
-scores_df <- read.table(paste(wkdir, "/peptides_scores.txt", sep=""), header=TRUE, sep="\t")
+dG_2b4_df <- read.table(paste(wkdir, "/3QIB/3QIB_dGs.txt", sep=""), header=TRUE, sep="\t")
+dG_226_df <- read.table(paste(wkdir, "/3QIU/3QIU_dGs.txt", sep=""), header=TRUE, sep="\t")
+dG_5cc7_df <- read.table(paste(wkdir, "/4P2R/4P2R_dGs.txt", sep=""), header=TRUE, sep="\t")
+scores_2b4_df <- read.table(paste(wkdir, "/3QIB/peptides_scores.txt", sep=""), header=TRUE, sep="\t")
+scores_226_df <- read.table(paste(wkdir, "/3QIU/peptides_scores.txt", sep=""), header=TRUE, sep="\t")
+scores_5cc7_df <- read.table(paste(wkdir, "/4P2R/peptides_scores.txt", sep=""), header=TRUE, sep="\t")
+# Collecct dGs
+dGs_2b4 <- dG_2b4_df$dG
+dGs_226 <- dG_226_df$dG
+dGs_5cc7 <- dG_5cc7_df$dG
+# Collect scores
+scores_2b4 <- scores_2b4_df$score
+scores_226 <- scores_226_df$score
+scores_5cc7 <- scores_5cc7_df$score
+# Aggregate
+dGs <- c(dGs_2b4, dGs_226, dGs_5cc7)
+scores <- c(scores_2b4, scores_226, scores_5cc7)
+#peptides <- scores_df$id
 
-scores <- scores_df$score
-dGs <- dG_df$dG
-peptides <- scores_df$id
+png(paste(wkdir,"/SPR_plot.png", sep=""), width=2000, height=2000, res = 300)
+	par(mar=c(5, 5, 5, 2) + 0.1, mgp=c(3,1,0),lwd=2)
+	plot(dGs_2b4, scores_2b4, xlab= expression(paste("Experimentally determined ",Delta, "G (kcal/mol)", sep="")),
+	cex.lab=1.5, cex.main=2.5, cex=1.5, ylab= expression(paste(Delta, "G", ""["BIND"], sep= "")), 
+	pch=21, col= "black", bg="red", xlim=c(-9.5,-4.5), ylim=c(-60,100),axes=FALSE)
+		points(dGs_226, scores_226, pch=22, col= "black", bg="blue",cex=1.5)
+		points(dGs_5cc7, scores_5cc7, pch=24, col= "black", bg="forestgreen",cex=1.5)
+	axis(1, lwd=2, cex.axis=1.3) 
+	axis(2, lwd=2, cex.axis=1.3)
+	box(bty="l")
 
-pdf(paste(wkdir,"/", args$pdb, "_SPR_plot.pdf", sep=""))
-	plot(dGs, scores, main= args$pdb, xlab= expression(paste("Experimentally determined ",Delta, "G (kcal/mol)", sep="")),
-	ylab= "Rosetta score", pch=16, col= "blue", xlim=c(-9,-4), ylim=c(-60,100))
-	text(dGs, scores, peptides, cex=0.7, pos=3)
+	#text(dGs, scores, peptides, cex=0.7, pos=3)
+	#text(-8.5, 75, paste("r = ", round(cor(dGs, scores), digits=2), sep=""), cex=1.7, pos=4)
+	legend(-9.5,100, c("2B4", "226", "5cc7"), col=rep("black",3), pt.bg=c("red", "blue", "forestgreen"), pch=c(21,22,24), cex=1.5)
 dev.off()
 
 # Print correlation
---
+++ figures/histograms/TCR_histogram_rnds.R
@@ -0,0 +1,122 @@
+#!/usr/bin/env Rscript
+library(argparse)
+parser <- ArgumentParser(description="Make histograms for bind_pep_alone Rosetta 3.5 scoring results")
+
+args <- parser$parse_args()
+wkdir <- getwd()
+
+png(paste(wkdir, "/TCR_histogram_rnds.png", sep=""), width=4000, height=3000, res=300)
+par(mfrow=c(4,5), oma=c(6,8,5,2) + 0.1, mar=c(1,1,1,1) + 0.5)
+# default: par(oma=c(2,2,2,2), c(5, 4, 4, 2) + 0.1,  c(3, 1, 0))
+
+# 3QIB
+score_table_0 <- read.table(paste(wkdir, "/3QIB/preselection/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_1 <- read.table(paste(wkdir, "/3QIB/round_1/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_2 <- read.table(paste(wkdir, "/3QIB/round_2/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_3 <- read.table(paste(wkdir, "/3QIB/round_3/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_4 <- read.table(paste(wkdir, "/3QIB/round_4/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+
+hist(score_table_0$bind_pep_alone, breaks=seq(-200,5000, by=50), col='red', lty="blank", xlab="",
+	 	main="",xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+par(xpd=NA)
+title("Preselection", cex.main=2.5, line=1.5, font.main=1)
+par(xpd=FALSE)
+box(bty="l")
+hist(score_table_1$bind_pep_alone, breaks=seq(-200,5000, by=50), col='red', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+par(xpd=NA)
+title("Round 1", cex.main=2.5, line=1.5, font.main=1)
+par(xpd=FALSE)
+box(bty="l")
+hist(score_table_2$bind_pep_alone, breaks=seq(-200,5000, by=50), col='red', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+par(xpd=NA)
+title("Round 2", cex.main=2.5, line=1.5, font.main=1)
+par(xpd=FALSE)
+box(bty="l")
+hist(score_table_3$bind_pep_alone, breaks=seq(-200,5000, by=50), col='red', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+par(xpd=NA)
+title("Round 3", cex.main=2.5, line=1.5, font.main=1)
+par(xpd=FALSE)
+box(bty="l")
+hist(score_table_4$bind_pep_alone, breaks=seq(-200,5000, by=50), col='red', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+par(xpd=NA)
+title("Round 4", cex.main=2.5, line=1.5, font.main=1)
+par(xpd=FALSE)
+box(bty="l")
+
+
+# 3QIU
+score_table_0 <- read.table(paste(wkdir, "/3QIU/preselection/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_1 <- read.table(paste(wkdir, "/3QIU/round_1/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_2 <- read.table(paste(wkdir, "/3QIU/round_2/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_3 <- read.table(paste(wkdir, "/3QIU/round_3/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_4 <- read.table(paste(wkdir, "/3QIU/round_4/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+
+hist(score_table_0$bind_pep_alone, breaks=seq(-200,5000, by=50), col='blue', lty="blank", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+hist(score_table_1$bind_pep_alone, breaks=seq(-200, 5000, by= 50), col='blue', lty="blank", xlab="",
+     main="", xlim=c(-200, 3500), cex.lab=2, cex.main=2,cex.axis=1.5)
+box(bty="l")
+hist(score_table_2$bind_pep_alone, breaks=seq(-200,5000, by=50), col='blue', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+hist(score_table_3$bind_pep_alone, breaks=seq(-200,5000, by=50), col='blue', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+hist(score_table_4$bind_pep_alone, breaks=seq(-200,5000, by=50), col='blue', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+
+# 4P2R
+score_table_0 <- read.table(paste(wkdir, "/4P2R/preselection/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_1 <- read.table(paste(wkdir, "/4P2R/round_1/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_2 <- read.table(paste(wkdir, "/4P2R/round_2/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_3 <- read.table(paste(wkdir, "/4P2R/round_3/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_4 <- read.table(paste(wkdir, "/4P2R/round_4/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+
+hist(score_table_0$bind_pep_alone, breaks=seq(-1000,7000, by=50), col='forestgreen', lty="blank", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2,cex.axis=1.5)
+box(bty="l")
+hist(score_table_1$bind_pep_alone, breaks=seq(-1000,7000, by=50), col='forestgreen', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2,cex.axis=1.5)
+box(bty="l")
+hist(score_table_2$bind_pep_alone, breaks=seq(-1000,7000, by=50), col='forestgreen', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2,cex.axis=1.5)
+box(bty="l")
+hist(score_table_3$bind_pep_alone, breaks=seq(-1000,7000, by=50), col='forestgreen', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2,cex.axis=1.5)
+box(bty="l")
+hist(score_table_4$bind_pep_alone, breaks=seq(-1000,7000, by=50), col='forestgreen', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-200,3500), cex.lab=2, cex.main=2,cex.axis=1.5)
+box(bty="l")
+
+# 1YMM
+score_table_0 <- read.table(paste(wkdir, "/1YMM/preselection/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_1 <- read.table(paste(wkdir, "/1YMM/round_1/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_2 <- read.table(paste(wkdir, "/1YMM/round_2/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_3 <- read.table(paste(wkdir, "/1YMM/round_3/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+score_table_4 <- read.table(paste(wkdir, "/1YMM/round_4/all/score_table.txt", sep=""), header=TRUE, sep="\t")
+
+hist(score_table_0$bind_pep_alone, breaks=seq(-500, 4000, by=30), col='mediumorchid4', lty="blank", xlab="",
+		main="", xlim=c(-100,2200), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+hist(score_table_1$bind_pep_alone, breaks=seq(-500, 4000, by=30), col='mediumorchid4', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-100,2200), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+hist(score_table_2$bind_pep_alone, breaks=seq(-500, 4000, by=30), col='mediumorchid4', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-100,2200), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+hist(score_table_3$bind_pep_alone, breaks=seq(-500, 4000, by=30), col='mediumorchid4', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-100,2200), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+hist(score_table_4$bind_pep_alone, breaks=seq(-500, 4000, by=30), col='mediumorchid4', lty="blank", ylab="", xlab="",
+		main="", xlim=c(-100,2200), cex.lab=2, cex.main=2, cex.axis=1.5)
+box(bty="l")
+
+
+title(xlab=expression(paste(Delta, "G", ""["BIND"], sep= "")), ylab="Number of peptides", outer=TRUE, cex.lab=2.5, line=3)
+dev.off()
