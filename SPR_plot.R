e359f7001c23afd9dd7e47eb6afe0e99e1bea0f1
borrmant <tylerborrman@gmail.com>
2020-07-06 17:54:11 -0400

    updated SPR plot after relax

--- SPR_plot.R
+++ SPR_plot.R
@@ -43,6 +43,22 @@ png(paste(wkdir,"/SPR_plot.png", sep=""), width=2000, height=2000, res = 300)
 	legend(-9.5,100, c("2B4", "226", "5cc7"), col=rep("black",3), pt.bg=c("red", "blue", "forestgreen"), pch=c(21,22,24), cex=1.5)
 dev.off()
 
+pdf(paste(wkdir,"/SPR_plot.pdf", sep=""), width=7, height=7)
+par(mar=c(5, 5, 5, 2) + 0.1, mgp=c(3,1,0),lwd=2)
+plot(dGs_2b4, scores_2b4, xlab= expression(paste("Experimentally determined ",Delta, "G (kcal/mol)", sep="")),
+     cex.lab=1.5, cex.main=2.5, cex=1.5, ylab= expression(paste(Delta, "G", ""["BIND"], sep= "")), 
+     pch=21, col= "black", bg="red", xlim=c(-9.5,-4.5), ylim=c(-60, 50),axes=FALSE)
+points(dGs_226, scores_226, pch=22, col= "black", bg="blue",cex=1.5)
+points(dGs_5cc7, scores_5cc7, pch=24, col= "black", bg="forestgreen",cex=1.5)
+axis(1, lwd=2, cex.axis=1.3) 
+axis(2, lwd=2, cex.axis=1.3)
+box(bty="l")
+
+#text(dGs, scores, peptides, cex=0.7, pos=3)
+#text(-8.5, 75, paste("r = ", round(cor(dGs, scores), digits=2), sep=""), cex=1.7, pos=4)
+legend(-9.5,100, c("2B4", "226", "5cc7"), col=rep("black",3), pt.bg=c("red", "blue", "forestgreen"), pch=c(21,22,24), cex=1.5)
+dev.off()
+
 # Print correlation
 print(paste("Correlation: ", cor(dGs, scores), sep=""))
 print(paste("Correlation 2B4: ", cor(dGs_2b4, scores_2b4), sep=""))
